77ca2fbd8b18a152d981b87f90da2b9f8a2f4a3c
vagrant <vagrant@vagrant.vm>
2020-10-22 20:21:08 +0000

    added install script for ansible

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 echo "Installing Terraform"
 sudo apt-get install curl -y
 export VER="0.13.2"
-cd
+cd 
 curl https://releases.hashicorp.com/terraform/${VER}/terraform_${VER}_linux_amd64.zip --output terraform_${VER}_linux_amd64.zip
 sudo apt-get install -y unzip
 unzip terraform_${VER}_linux_amd64.zip
